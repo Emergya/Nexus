270daf12e86c5818c9c3f79ec8d50a0a7973aa83
dmartin <dmartin@emergya.com>
2014-05-02 11:28:03 +0200

    modal

---
+++ templates/views-view-field--modal--body.tpl.php
@@ -0,0 +1,11 @@
+      <div class="modal-body">
+        <?php print $output; $_SESSION['count']=$_SESSION['count'] + 1; ?>
+      </div>
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
---
+++ templates/views-view-unformatted--modal.tpl.php
@@ -0,0 +1,16 @@
+<? 
+$_SESSION['count'] = 1;
+?>
+
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<?php foreach ($rows as $id => $row): ?>
+
+
+
+      <?php print $row; ?>
+    
+
+  
+<?php endforeach; ?>
