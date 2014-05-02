4e4ad4f2bb1d1a708fb5b433e7a428f753616ded
dmartin <dmartin@emergya.com>
2014-05-02 11:26:26 +0200

    full-page

---
+++ templates/views-view-field--scrollspyv2--title.tpl.php
@@ -0,0 +1,7 @@
+<div class="section " id=<?php print "section{$_SESSION['count']}" ?> >
+	<div class="wrap">
+			<div class="box">
+  <h1>
+    <?php print $output; $_SESSION['count']=$_SESSION['count'] + 1; ?>
+  </h1>
+<? /*<--! easing.html -->*/ ?>
